49f5614773e5cc0325b981856ceb8631b3f6c553
zhangxiaotin <zhangxiaotin@126.com>
2019-02-25 17:21:16 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
             .rule("images")
             .use("url-loader")
             .loader("url-loader")
-            .tap(options => Object.assign(options, { limit: 10240 }));
+            .tap(options => Object.assign(options, { limit: 20480 }));
     },
     configureWebpack: {
         output: {
